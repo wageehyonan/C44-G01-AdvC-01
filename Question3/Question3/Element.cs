df75e995c6e9a94fef0032fa15d09e4d9b90abf3
wageehyonan <wageehyonan15@gmail.com>
2025-08-06 05:59:36 -0700

    Add files via upload

---
+++ Question3/Question3/Element.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Question3
+{
+    public  class Element <T>
+        {
+
+        #region Parameters
+        private T elem;
+        #endregion
+        #region Properits
+        public T Elem
+        {
+            get { return elem; }
+            set { elem = value; }
+        }
+
+        #endregion
+        #region Constructors
+        public Element(T el)
+        { this.elem = el; }
+        #endregion
+
+
+        #region ToString
+        public override string ToString()
+        {
+            return $"Element IS {Elem}";
+        }
+        #endregion
+
+        public static void Reverse_Element(T []arr)
+        {
+            for (int i=arr.Length-1;i>=0;i--)
+            {
+                Console.WriteLine($"Element Number {i} IS {arr[i]}");
+            }
+        }
+    }
+}
---
+++ Question3/Question3/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace Question3
+{
+    internal class Program
+    {/*
+     1.	You are given an ArrayList containing a sequence of elements. try to reverse the order of elements in the ArrayList
+        in-place(in the same arrayList) without using the built-in Reverse. Implement a function that takes the ArrayList as
+        input and modifies it to have the reversed order of elements. 
+      */
+        static void Main(string[] args)
+        {
+            Element<int> elm1 = new Element<int>(5);
+            Console.WriteLine(elm1);
+
+            Element<string> elm2 = new Element<string>("Test String Type");
+            Console.WriteLine(elm2);
+
+            Element<int>[] arr_el ={new Element<int>(5),
+                                   new Element<int>(6),
+                                   new Element<int>(7),
+                                   new Element<int>(8),
+                                   new Element<int>(9),
+                                   new Element<int>(10),
+                                   };
+           
+            int[] arr_ele = { 5, 6, 7, 8, 9, 10 };
+            Element<int>.Reverse_Element(arr_ele);
+
+
+        }
+}
+}
