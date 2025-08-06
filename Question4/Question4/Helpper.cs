fd109eb6e62cb0c8cc4af9d509e9c756622b2271
wageehyonan <wageehyonan15@gmail.com>
2025-08-06 06:00:20 -0700

    Add files via upload

---
+++ Question4/Question4/Helpper.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Question4
+{
+    public static class Helpper
+    {
+        public static void NewList_Even(int[] arr)
+        {
+            Console.WriteLine("Even Numbers are :");
+            for (int i = 0; i < arr.Length; i++)
+            {
+                if ((arr[i]%2) == 0)
+                {
+                    Console.WriteLine(arr[i]+"\n");
+                }
+            }
+        }
+    }
+}
---
+++ Question4/Question4/Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+
+namespace Question4
+{
+    internal class Program
+    {
+       //4.	You are given a list of integers.Your task is to find and return a 
+       //     new list containing only the even numbers from the given list.
+        static void Main(string[] args)
+        {
+            int[] arr = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14 };
+            Helpper.NewList_Even(arr);
+        }
+    }
+}
